f2524be0514e4b1de452c8d29987deeb0cacd279
Jose Silvera <josesilvera1414@gmail.com>
2022-12-07 20:20:28 -0300

    adding gitignore

---
+++ part1/src/index.js
@@ -0,0 +1,9 @@
+import React, {createRoot} from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <App />
+)
